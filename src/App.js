b9a9a3bc52d0b2238281f46a3cbae9522d7f03bf
chandni satpute <chandnisatpue@gmail.com>
2025-04-18 15:40:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ClassComp from './ClassComp';
+
+function App() {
+  return (
+    <div className="app-container">
+      < ClassComp />
+    </div>
+  );
+}
+
+export default App;
